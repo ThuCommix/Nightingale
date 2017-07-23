3d4b7847616a36a76da126d47d52eeac6d8532f9
Kevin Scholz <thucommix@gmx.net>
2017-07-23 11:29:42 +0200

    Removed db services

--- Concordia.Framework/DbServices/IDbService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Concordia.Framework.DbServices
-{
-    [Obsolete("The DbServices namespace with it's content will be removed in v2.0.0 to accomplish .net standard 2.0 compatibility.")]
-    public interface IDbService
-    {
-    }
-}
