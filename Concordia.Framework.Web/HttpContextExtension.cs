a204c2b3cc28a9ee6d5027d5033a8d854b63a3e6
Kevin Scholz <thucommix@gmx.net>
2017-08-01 23:54:03 +0200

    Added better session handling for ASP.NET projects

---
+++ Concordia.Framework.Web/HttpContextExtension.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Concordia.Framework.Web
+{
+    internal static class HttpContextExtension
+    {
+        private static readonly Dictionary<Guid, HttpContext> _httpContexts = new Dictionary<Guid, HttpContext>();
+        private static readonly object _locker = new object();
+
+        /// <summary>
+        /// Sets a unique identifier for this http context.
+        /// </summary>
+        /// <param name="httpRequest">The http request.</param>
+        public static void SetUniqueIdentifier(this HttpContext httpContext)
+        {
+            lock (_locker)
+            {
+                ClearHttpRequests();
+
+                _httpContexts.Add(Guid.NewGuid(), httpContext);
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique identifier for this http request.
+        /// </summary>
+        /// <param name="httpRequest">The http context.</param>
+        /// <returns>Returns a unique identifier.</returns>
+        public static Guid GetUniqueIdentifier(this HttpContext httpContext)
+        {
+            lock (_locker)
+            {
+                ClearHttpRequests();
+
+                if (!_httpContexts.ContainsValue(httpContext))
+                    return Guid.Empty;
+
+                return _httpContexts.FirstOrDefault(x => x.Value == httpContext).Key;
+            }
+        }
+
+        /// <summary>
+        /// Clears http requests which are not valid anymore.
+        /// </summary>
+        private static void ClearHttpRequests()
+        {
+            foreach(var httpRequestEntry in _httpContexts.ToList())
+            {
+                if(!httpRequestEntry.Value?.Response?.IsClientConnected ?? false)
+                {
+                    _httpContexts.Remove(httpRequestEntry.Key);
+                }
+            }
+        }
+    }
+}
