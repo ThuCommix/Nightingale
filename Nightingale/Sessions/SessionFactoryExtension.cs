4a6a3c234f591e86f3add6832cf9074e675884a1
Kevin Scholz <thucommix@gmx.net>
2017-12-18 22:49:58 +0100

    #7 add extension methods for opening a session async

---
+++ Nightingale/Sessions/SessionFactoryExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace Nightingale.Sessions
+{
+    /// <summary>
+    /// Provides async functionality for the <see cref="ISessionFactory"/>.
+    /// </summary>
+    public static class SessionFactoryExtension
+    {
+        /// <summary>
+        /// Opens a new session async.
+        /// </summary>
+        /// <param name="sessionFactory">The session factory.</param>
+        /// <returns>Returns a session instance.</returns>
+        public static Task<ISession> OpenSessionAsync(this ISessionFactory sessionFactory)
+        {
+            return Task.Run(() => sessionFactory.OpenSession());
+        }
+
+        /// <summary>
+        /// Opens a new session.
+        /// </summary>
+        /// <typeparam name="T">The session type.</typeparam>
+        /// <param name="sessionFactory">The session factory.</param>
+        /// <returns>Returns a session instance.</returns>
+        public static Task<ISession> OpenSessionAsync<T>(this ISessionFactory sessionFactory) where T : ISession
+        {
+            return Task.Run(() => sessionFactory.OpenSession<T>());
+        }
+    }
+}
