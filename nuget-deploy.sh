2f2314040d9b806f5349ef019278de6eb6ec6226
Kevin Scholz <thucommix@gmx.net>
2017-12-21 14:45:38 +0100

    Do not fail build when nuget package exists

--- nuget-deploy.sh
+++ nuget-deploy.sh
@@ -3,3 +3,5 @@ Source=$2
 
 dotnet pack ./Nightingale/Nightingale.csproj --include-symbols
 dotnet nuget push ./Nightingale/bin/Debug/Nightingale.*.symbols.nupkg --source $Source --api-key $ApiKey
+
+exit 0
