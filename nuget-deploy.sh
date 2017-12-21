af4e51e3062f2bc5d7b7979848a8dbeef6479043
Kevin Scholz <thucommix@gmx.net>
2017-12-21 14:34:39 +0100

    Add auto nuget push for develop branch

---
+++ nuget-deploy.sh
@@ -0,0 +1,5 @@
+ApiKey=$1
+Source=$2
+
+dotnet pack ./Nightingale/Nightingale.csproj --include-symbols
+dotnet nuget push ./Nightingale/bin/Debug/Nightingale.*.symbols.nupkg --source $Source --api-key $ApiKey
